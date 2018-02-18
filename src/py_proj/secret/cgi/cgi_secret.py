e03afea2b056e3644d0e9e9e4a51eceee19cb327
340StarObserver <lvyang@ippclub.org>
2018-02-18 23:15:30 +0800

    feat ( secret ) : operateSecret, remarkSecret, getSecretList, getSecretKey

--- src/py_proj/secret/cgi/cgi_secret.py
+++ src/py_proj/secret/cgi/cgi_secret.py
@@ -124,3 +124,240 @@ def cgi_createSecret(para, res):
     finally:
         if session_mark is True:
             session_auth.close()
+
+
+def _modify_secret_status(session_auth, secret_id, op_mode):
+    res = {
+        "secretId"  : secret_id,
+        "opCode"    : conf_code.CONF_CODE["success"][0],
+        "opMessage" : conf_code.CONF_CODE["success"][1]
+    }
+
+    obj_secret = session_auth.query(TableSecretKey).filter(TableSecretKey.secretId == secret_id).first()
+
+    if obj_secret is None:
+        res["opCode"]    = conf_code.CONF_CODE["invalid_secret_id"][0]
+        res["opMessage"] = conf_code.CONF_CODE["invalid_secret_id"][1]
+        return res
+
+    if   op_mode == SECRET_STATUS_ENABLE:
+        if int(obj_secret.status) == SECRET_STATUS_DISENABLE:
+            obj_secret.status  = SECRET_STATUS_ENABLE
+            obj_secret.modTime = datetime.datetime.now()
+        else:
+            res["opCode"]    = conf_code.CONF_CODE["invalid_secret_status_enable"][0]
+            res["opMessage"] = conf_code.CONF_CODE["invalid_secret_status_enable"][1]
+    elif op_mode == SECRET_STATUS_DISENABLE:
+        if int(obj_secret.status) == SECRET_STATUS_ENABLE:
+            obj_secret.status  = SECRET_STATUS_DISENABLE
+            obj_secret.modTime = datetime.datetime.now()
+        else:
+            res["opCode"]    = conf_code.CONF_CODE["invalid_secret_status_disenable"][0]
+            res["opMessage"] = conf_code.CONF_CODE["invalid_secret_status_disenable"][1]
+    elif op_mode == SECRET_STATUS_DELETE:
+        if int(obj_secret.status) == SECRET_STATUS_DISENABLE:
+            obj_secret.status  = SECRET_STATUS_DELETE
+            obj_secret.modTime = datetime.datetime.now()
+        else:
+            res["opCode"]    = conf_code.CONF_CODE["invalid_secret_status_delete"][0]
+            res["opMessage"] = conf_code.CONF_CODE["invalid_secret_status_delete"][1]
+
+    return res
+
+
+def cgi_operateSecret(para, res):
+    # 1. prepare a logger
+    logger = global_tool.get_logger(sys._getframe().f_code.co_name)
+
+    # 2. get parameters
+    if "opMode" not in para:
+        res["returnCode"]    = conf_code.CONF_CODE["para_miss"][0]
+        res["returnMessage"] = "miss parameter(opMode)"
+        return
+
+    if  int(para["opMode"]) != SECRET_STATUS_ENABLE    and \
+        int(para["opMode"]) != SECRET_STATUS_DISENABLE and \
+        int(para["opMode"]) != SECRET_STATUS_DELETE:
+        res["returnCode"]    = conf_code.CONF_CODE["invalid_op_mode"][0]
+        res["returnMessage"] = conf_code.CONF_CODE["invalid_op_mode"][1]
+        return
+
+    opMode = int(para.get("opMode", 0))
+    secretIds  = para.get("secretIds", [])
+
+    if not isinstance(secretIds, list):
+        secretIds = [secretIds]
+
+    n = len(secretIds)
+    i = 0
+    while i < n:
+        secretIds[i] = global_tool.deal_post_str(secretIds[i])
+        i += 1
+
+    # 3. logic work
+    session_mark = False
+    try:
+        session_auth = global_db.DB_SESSION_AUTH
+        session_mark = True
+
+        res["data"]["batchRes"] = []
+        for secret_id in secretIds:
+            res["data"]["batchRes"].append(_modify_secret_status(session_auth, secret_id, opMode))
+
+        session_auth.commit()
+
+    except Exception, e:
+        if session_mark is True:
+            session_auth.rollback()
+
+        traceback_str = traceback.format_exc()
+        res["returnCode"]    = conf_code.CONF_CODE["sql_error"][0]
+        res["returnMessage"] = traceback_str
+
+        logger.error(traceback_str)
+
+    finally:
+        if session_mark is True:
+            session_auth.close()
+
+
+def cgi_remarkSecret(para, res):
+    # 1. prepare a logger
+    logger = global_tool.get_logger(sys._getframe().f_code.co_name)
+
+    # 2. get parameters
+    if "secretId" not in para:
+        res["returnCode"]    = conf_code.CONF_CODE["para_miss"][0]
+        res["returnMessage"] = "miss parameter(secretId)"
+        return
+
+    if "secretRemark" not in para:
+        res["returnCode"]    = conf_code.CONF_CODE["para_miss"][0]
+        res["returnMessage"] = "miss parameter(secretRemark)"
+        return
+
+    secretId     = global_tool.deal_post_str(para["secretId"])
+    secretRemark = global_tool.deal_post_str(para["secretRemark"])
+
+    # 3. logic work
+    session_mark = False
+    try:
+        session_auth = global_db.DB_SESSION_AUTH
+        session_mark = True
+
+        obj_secret = session_auth.query(TableSecretKey).filter(TableSecretKey.secretId == secretId).first()
+
+        if obj_secret is None:
+            res["returnCode"]    = conf_code.CONF_CODE["invalid_secret_id"][0]
+            res["returnMessage"] = conf_code.CONF_CODE["invalid_secret_id"][1]
+        else:
+            obj_secret.secretRemark = secretRemark
+            obj_secret.modTime      = datetime.datetime.now()
+
+        session_auth.commit()
+
+    except Exception, e:
+        if session_mark is True:
+            session_auth.rollback()
+
+        traceback_str = traceback.format_exc()
+        res["returnCode"]    = conf_code.CONF_CODE["sql_error"][0]
+        res["returnMessage"] = traceback_str
+
+        logger.error(traceback_str)
+
+    finally:
+        if session_mark is True:
+            session_auth.close()
+
+
+def cgi_getSecretList(para, res):
+    # 1. prepare a logger
+    logger = global_tool.get_logger(sys._getframe().f_code.co_name)
+
+    # 2. get parameters
+    if "userUin" not in para:
+        res["returnCode"]    = conf_code.CONF_CODE["para_miss"][0]
+        res["returnMessage"] = "miss parameter(userUin)"
+        return
+
+    userUin  = int(para.get("userUin" , 0))
+    showMode = int(para.get("showMode", 0))
+
+    # 3. logic work
+    session_mark = False
+    try:
+        session_auth = global_db.DB_SESSION_AUTH
+        session_mark = True
+
+        secret_list = session_auth.query(TableSecretKey).filter(
+            TableSecretKey.userUin == userUin,
+            TableSecretKey.status  != SECRET_STATUS_DELETE
+        ).all()
+
+        res["data"]["secretList"] = []
+
+        for obj_secret in secret_list:
+            json_secret = obj_secret.to_json()
+            if showMode != SECRET_SHOW_MODE and "secretKey" in json_secret:
+                json_secret.pop("secretKey")
+            res["data"]["secretList"].append(json_secret)
+
+        session_auth.commit()
+
+    except Exception, e:
+        if session_mark is True:
+            session_auth.rollback()
+
+        traceback_str = traceback.format_exc()
+        res["returnCode"]    = conf_code.CONF_CODE["sql_error"][0]
+        res["returnMessage"] = traceback_str
+
+        logger.error(traceback_str)
+
+    finally:
+        if session_mark is True:
+            session_auth.close()
+
+
+def cgi_getSecretKey(para, res):
+    # 1. prepare a logger
+    logger = global_tool.get_logger(sys._getframe().f_code.co_name)
+
+    # 2. get parameters
+    if "secretId" not in para:
+        res["returnCode"]    = conf_code.CONF_CODE["para_miss"][0]
+        res["returnMessage"] = "miss parameter(secretId)"
+        return
+
+    secretId = global_tool.deal_post_str(para["secretId"])
+
+    # 3. logic work
+    session_mark = False
+    try:
+        session_auth = global_db.DB_SESSION_AUTH
+        session_mark = True
+
+        obj_secret = session_auth.query(TableSecretKey).filter(TableSecretKey.secretId == secretId).first()
+
+        if obj_secret is None:
+            res["returnCode"]    = conf_code.CONF_CODE["invalid_secret_id"][0]
+            res["returnMessage"] = conf_code.CONF_CODE["invalid_secret_id"][1]
+        else:
+            res["data"]["secretDetail"] = obj_secret.to_json()
+
+        session_auth.commit()
+
+    except Exception, e:
+        if session_mark is True:
+            session_auth.rollback()
+
+        traceback_str = traceback.format_exc()
+        res["returnCode"]    = conf_code.CONF_CODE["sql_error"][0]
+        res["returnMessage"] = traceback_str
+
+        logger.error(traceback_str)
+
+    finally:
+        if session_mark is True:
+            session_auth.close()
--- src/py_proj/secret/conf/conf_code.py
+++ src/py_proj/secret/conf/conf_code.py
@@ -3,13 +3,16 @@
 # ----------------------------------------
 # About error code
 CONF_CODE = {
-    "success"           : ( 0  , "ok"                        ),
-    "unknown"           : ( 101, "unknown error"             ),
-    "invalid_method"    : ( 102, "invalid interfaceName"     ),
-    "para_miss"         : ( 103, "miss parameter"            ),
-    "para_error"        : ( 104, "para parse error"          ),
-    "sql_error"         : ( 105, "exec sql error"            ),
-    "invalid_user_uin"  : ( 1001, "no such userUin"          ),
-    "invalid_op_mode"   : ( 1101, "opMode can only be 0/1/2" ),
-    "invalid_secret_id" : ( 1201, "no such secret"           )
+    "success"                         : ( 0   , "ok"                                    ),
+    "unknown"                         : ( 101 , "unknown error"                         ),
+    "invalid_method"                  : ( 102 , "invalid interfaceName"                 ),
+    "para_miss"                       : ( 103 , "miss parameter"                        ),
+    "para_error"                      : ( 104 , "para parse error"                      ),
+    "sql_error"                       : ( 105 , "exec sql error"                        ),
+    "invalid_user_uin"                : ( 1001, "no such userUin"                       ),
+    "invalid_op_mode"                 : ( 1101, "opMode can only be 0/1/2"              ),
+    "invalid_secret_id"               : ( 1102, "no such secret"                        ),
+    "invalid_secret_status_enable"    : ( 1103, "only disenabled secret can be enabled" ),
+    "invalid_secret_status_disenable" : ( 1104, "only enabled secret can be disenabled" ),
+    "invalid_secret_status_delete"    : ( 1105, "only disenabled secret can be deleted" )
 }
